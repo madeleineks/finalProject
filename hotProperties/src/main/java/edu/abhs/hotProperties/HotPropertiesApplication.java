1ebc62cf00aa5b191a5e5d5c1370b77c94703a47
Madeleine Santos <madeleinee.santos@gmail.com>
2025-05-26 12:23:16 -0500

    Created hotProperties project

---
+++ hotProperties/src/main/java/edu/abhs/hotProperties/HotPropertiesApplication.java
@@ -0,0 +1,13 @@
+package edu.abhs.hotProperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotPropertiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotPropertiesApplication.class, args);
+	}
+
+}
---
+++ hotProperties/src/test/java/edu/abhs/hotProperties/HotPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.abhs.hotProperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotPropertiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

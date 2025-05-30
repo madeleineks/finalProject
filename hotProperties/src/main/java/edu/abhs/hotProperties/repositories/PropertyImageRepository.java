48c9d4e3364d72e15bebc4cdf806d6d8aa5b670e
ZachBaccaro <81643518+ZachBaccaro@users.noreply.github.com>
2025-05-30 18:40:49 -0400

    Revert "Merge branch 'Zach's-branch' into Hamzeh's-branch"
    This reverts commit d869690380baf8261dfb2de13a7c110662cef431, reversing
    changes made to b91bd3207078febb69c72be1857d13f7d4dd3388.

--- hotProperties/src/main/java/edu/abhs/hotProperties/repositories/PropertyImageRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.abhs.hotProperties.repositories;
-
-import edu.abhs.hotProperties.entities.PropertyImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PropertyImageRepository extends JpaRepository<PropertyImage, Long> {
-
-}
--- hotProperties/src/main/java/edu/abhs/hotProperties/repositories/PropertyRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.abhs.hotProperties.repositories;
-
-import edu.abhs.hotProperties.entities.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-
-}
